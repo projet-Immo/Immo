a97eacac2bb76dedabd68b2f95cc8a72b7436020
Abygueye11 <gueyeadjiaby@gmail.com>
2025-07-03 16:48:41 +0000

    Objectif

--- src/app/views/pages/rse/rse-routing.module.ts
+++ src/app/views/pages/rse/rse-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RseListProjetComponent } from './rse-list-projet/rse-list-projet.component';
 import { SuiviProjetComponent } from './projet/suivi-projet/suivi-projet.component';
+import { ObjectifListeComponent } from './Objestif/objectif-liste/objectif-liste.component';
 
 const routes: Routes = [
   {path: '', component: RseListProjetComponent},
   {path: 'projet/:projetName', component: SuiviProjetComponent},
+  {path: 'object', component: ObjectifListeComponent},
+
 ];
 
 @NgModule({
